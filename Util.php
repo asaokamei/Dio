9427747964c4d25197d85f2fd7971c3caf7f0ac8
kamei@workspot.jp <asao@asaoMBAir.local>
2011-11-13 17:44:36 +0900

    first try to create utility class.

---
+++ Util.php
@@ -0,0 +1,43 @@
+<?php
+namespace CenaDta\Dio;
+
+class Util
+{
+	// +--------------------------------------------------------------- +
+	/** checks if a variable ($var) have a value (non empty value).
+	 *  returns TRUE if $var==0 as well.
+	 */
+	static isValue( $var, $name=FALSE ) {
+		if( is_array( $var ) ) { 
+			if( $name === FALSE ) {
+				return( count( $var ) ); 
+			}
+			if( isset( $var[ $name ] ) ) {
+				return have_value( $var[ $name ] );
+			}
+			return FALSE;
+		}
+		else
+		if( is_object( $var ) ) {
+			return TRUE;
+		}
+		else
+		if( "$var" == "" ) { 
+			return FALSE; 
+		}
+		else { 
+			return TRUE; 
+		}
+	}
+	// +--------------------------------------------------------------- +
+	/** gets a value in $arr[ $name ] without causing E_NOTICE error.
+	 */
+	static function getValue( $arr, $name, $default=FALSE ) {
+		if( isset( $arr[ $name ] ) ) {
+			returns $arr[ $name ];
+		}
+		return $default;
+	}
+}
+
+?>
