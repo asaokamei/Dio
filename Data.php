4d9b06d06e71cbfe5b246ced2efa6fc883c68bb3
Asao Kamei <kamei@workspot.jp>
2011-11-25 15:09:21 +0900

    added bunchs of comments.

--- Data.php
+++ Data.php
@@ -12,6 +12,10 @@ class Data
 	var $err_msg  = array();
 	var $err_num  = 0;
 	// +--------------------------------------------------------------- +
+    /**
+     * constructor
+     * @param string $save_id    used as save_id name. 
+     */
 	function __construct( $save_id=NULL ) {
 		$this->setSaveId( $save_id );
         $this->data_source = array(
@@ -23,18 +27,25 @@ class Data
 	}
 	// +--------------------------------------------------------------- +
     /**
-     * 
+     * set save_id.
+     * @param string $save_id    sets save_id.
+     * @return $this
      */
     function setSaveId( $save_id ) {
 		if( Util::isValue( $save_id ) ) $this->save_id = $save_id;
         return $this;
     }
-
-// +--------------------------------------------------------------- +
+    // +--------------------------------------------------------------- +
     /**
+     * find variable from $this->data_source. 
      * 
+     * @param mix $value    returns found value. 
+     * @param string $name  name of value to find.
+     * @param string $type  type of value to find.
+     * @param array $filter extra filter.
+     * @return string       returns found source, or FALSE if not found.
      */
-    function find( &$value, $name, $type, $filter=array() ) {
+    function find( &$value, $name, $type='asis', $filter=array() ) {
         $found = FALSE;// not found
         $value = NULL; // not found
         foreach( $this->data_source as $name => $source ) {
@@ -49,6 +60,15 @@ class Data
 	// +--------------------------------------------------------------- +
 	//  push and pop
 	// +--------------------------------------------------------------- +
+    /**
+     * find variable and set to $this->data[$name].
+     * if error, set to $this->err_msg[$name].
+     * 
+     * @param type $name    name of value to find.
+     * @param type $type    type of value to find.
+     * @param type $filter  extra filter.
+     * @return $this
+     */
 	function push( $name, $type='asis', $filter=array() ) {
         $found = $this->find( $value, $name, $type, $filter );
         if( Validator::validate( $value, $type, $filter, $error ) ) {
@@ -60,19 +80,35 @@ class Data
 		return $this;
 	}
 	// +--------------------------------------------------------------- +
+    /**
+     * pop $name value. same as get. 
+     * @param type $name  name of value. 
+     * @return mix        the value.
+     */
 	function pop( $name=FALSE ) {
 		return $this->get( $name );
 	}
 	// +--------------------------------------------------------------- +
+    /**
+     * pop $name for HTML display (htmlspecialchars'ed). 
+     * @param type $name  name of value.
+     * @return type       the html-safe value. 
+     */
 	function popHtml( $name=FALSE ) {
-		$return = $this->data;
+		$return = $this->pop( $name );
 		array_walk_recursive( $return, 
 			function( &$v, $k ) { $v = htmlspecialchars( $v, ENT_QUOTES ); } 
 		);
 		return $return;
 	}
 	// +--------------------------------------------------------------- +
-	function isError( $name, &$err_msg ) {
+    /**
+     * checks if error for given $name. 
+     * @param type $name      name of value to check for error.
+     * @param type $err_msg   error message if error. 
+     * @return boolean        TRUE if error, FALSE if not an error. 
+     */
+	function isError( $name, &$err_msg=NULL ) {
 		if( $err_msg = $this->getError( $name ) ) {
 			return TRUE;
 		}
@@ -86,6 +122,12 @@ class Data
 	// +--------------------------------------------------------------- +
 	//  setter and getter
 	// +--------------------------------------------------------------- +
+    /**
+     * setter for $name. stores at $this->data[$name]. 
+     * @param type $name   name of value to set.
+     * @param type $value  the value. 
+     * @return $this.
+     */
 	function set( $name, $value ) {
 		if( is_array( $name ) ) {
 			$this->data = array_merge( $this->data, $name );
@@ -96,6 +138,12 @@ class Data
 		return $this;
 	}
 	// +--------------------------------------------------------------- +
+    /**
+     * getter for $name. if $name is not given, return all the data.
+     * @param type $name  name of value to get. 
+     * @return mix        returns the named value, 
+     *                     or all data if name is not given. 
+     */
 	function get( $name=FALSE ) {
 		if( $name === FALSE ) {
 			return $this->data;
@@ -106,6 +154,13 @@ class Data
 		return FALSE;
 	}
 	// +--------------------------------------------------------------- +
+    /**
+     * sets data source. 
+     * @param array $source  data source to use
+     * @param boolena $only  set to TRUE to use this source only. 
+     * @param string $title  title of data source (default: 'myData').
+     * @return $this
+     */
 	function setSource( $source, $only=FALSE, $title='myData' ) {
 		if( $only ) {
 			$this->data_source = array( $title => $source );
@@ -117,6 +172,13 @@ class Data
 		return $this;
 	}
 	// +--------------------------------------------------------------- +
+    /**
+     * sets errors. 
+     * @param type $name     name of value where error occured.
+     * @param type $err_msg  error message for the value. 
+     * @param type $value    errored value. 
+     * @return $this
+     */
 	function setError( $name, $err_msg, $value=FALSE ) {
 		$this->err_msg[ $name ] = $err_msg;
 		if( $value !== FALSE ) {
@@ -151,6 +213,7 @@ class Data
         if( isset( $_SESSION[ $this->save_id ] ) ) {
             unset( $_SESSION[ $this->save_id ] );
         }
+        return $this;
     }
 	// +--------------------------------------------------------------- +
     function savePost() {
