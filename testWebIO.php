092bf88f707710938360f21fcc700ae09041b194
kamei@workspot.jp <asao@asaoMBAir.local>
2011-11-26 17:56:41 +0900

    added test on {save|load}Post method.

--- testWebIO.php
+++ testWebIO.php
@@ -8,14 +8,33 @@ define( 'WORDY', 0 );
 /**
  * PHPUnit test for Web Input/Output object.
  */
-class Util_DataIOTest extends PHPUnit_Framework_TestCase
+class Util_WebIOTest extends PHPUnit_Framework_TestCase
 {
 	// +----------------------------------------------------------------------+
 	public function setUp()
 	{
 	}
 	// +----------------------------------------------------------------------+
-	public function test_WebIO()
+	public function test_saveLoadPost()
+	{
+        $data = array(
+            'test' => 'test',
+            'more' => 'more',
+        );
+        $encode_type = WebIO::ENCODE_NONE;
+        $save_id     = WebIO::$save_id;
+        
+        $encoded     = WebIO::encodeData( $data, $encode_type );
+        $enc_id      = WebIO::$encode_id . $save_id;
+        $html = WebIO::savePost( $data, NULL, $encode_type );
+        list( $html1, $html2 ) = explode( '><', $html );
+        
+        $this->assertContains( $save_id, $html1 );
+        $this->assertContains( $encoded, $html1 );
+        $this->assertContains( $enc_id,  $html2 );
+	}
+	// +----------------------------------------------------------------------+
+	public function test_en_de_code()
 	{
         $data = array(
             'test' => 'test',
