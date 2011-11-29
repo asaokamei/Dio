064ab7e3c0e645fbe558c0ff1280925330f1eff2
kamei@workspot.jp <asao@asaoMBAir.local>
2011-11-29 21:28:14 +0900

    renamed Vf.php to Verify.php.

--- Verify.php
+++ Verify.php
@@ -1,71 +1,61 @@
 <?php
-namespace CenaDta\Dio;
+namespace CenaDta\Util;
 
-class Verify
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Vf
 {
-	static $filter_classes = array( 'CenaDta\Dio\Verify' );
-	// +--------------------------------------------------------------- +
-	// BASIC METHODS 
-	// +--------------------------------------------------------------- +
-	/** main verify method for verifying value using this Verify class. 
-	 */ 
-	function verify( $value, &$error, $verify_name, $options ) {
-		$success = TRUE;
-		if( !have_value( $value ) ) {     // do not verify if empty. 
-			return $success;
-		}
-		else 
-		if( is_array( $value ) ) {        // do recursive verify on $val. 
-			if( !is_array( $error ) ) { // make sure $error is an array. 
-				$error = array(); 
-			}
-			foreach( $value as $key => $val ) {
-				$success &= self::verify( $val, $error[$key], $verify_name, $options );
-			}
-			return $success;
-		}
-		// verify value. 
-		foreach( self::$verify_classes as $class ) {
-			if( method_exists( $class, $verify_name ) ) {
-				$success = $class::$verify_name( $value, $options );
-			}
-		}
-		if( function_exists( $filter_name ) ) {
-			$success = $filter_name( $value, $options );
-		}
-		if( !$success ) { // it's an error. set an error message in $error.
-			if( isset( $options[ 'err_msg' ] ) ) {
-				$error = $options[ 'err_msg' ];
-			}
-			else {
-				$error = $verify_name;
-			}
-			if( WORDY ) echo "<font color=red>verify failed( $value, $error, $verify_name, $options );</font><br/>\n";
-		}
-		return $success;
-	}
-	// +--------------------------------------------------------------- +
-	/** add another class for filtering, such as FilterJa. 
-	 */
-	function setFilterClass( $class ) {
-		self::$filter_classes[] = $class;
-	}
-	// +--------------------------------------------------------------- +
-	// VERIFY METHODS!!!
-	// +--------------------------------------------------------------- +
-	function exist( $val, $option ) {
-		if( $option ) return have_value( $val );
-		return TRUE;
-	}
-	// +--------------------------------------------------------------- +
-	function regexp( $val, $option ) {
-		if( have_value( $val ) ) {
-			return preg_match( "/$option/", $val );
-		}
-		return TRUE;
-	}
-	// +--------------------------------------------------------------- +
+    /**
+     * source to search data for. 
+     * @var array
+     */
+    static $source;
+    /**
+     * found data
+     * @var array
+     */
+    static $data;
+    /**
+     * error message
+     * @var array
+     */
+    static $error;
+    /**
+     * number of errors
+     * @var integer
+     */
+    static $err_num;
+    // +--------------------------------------------------------------- +
+    static function source( $source ) {
+        if( !is_null( $source ) && is_array( $source ) ) {
+            self::$source = $source;
+        }
+    }
+    // +--------------------------------------------------------------- +
+    static function data( $data=NULL ) {
+        return self::$data;
+    }
+    // +--------------------------------------------------------------- +
+    static function push( $name, $type='asis', $filter=array() ) {
+        $error   = NULL;
+        $value   = NULL;
+        $success = Validator::find( self::$source, $name, $value, $type, $filter, $error );
+        if( !$success ) {
+            self::$err_num++;
+            self::$error[ $name ] = $error;
+        }
+        self::$data[ $name ] = $value;
+        return $success;
+    }
+    // +--------------------------------------------------------------- +
+    static function isError( &$error=NULL ) {
+        $error = self::$error;
+        return !self::$err_num;
+    }
+    // +--------------------------------------------------------------- +
 }
-
 
 ?>
