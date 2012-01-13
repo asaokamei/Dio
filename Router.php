b2b37866d30f29d42ee3417017d8b46fedc98a45
kamei@workspot.jp <asao@asaoMBAir.local>
2012-01-13 23:29:58 +0900

    really simple/dumb router class... and a sample base{Loader|View} class to be dispatched.

---
+++ Router.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * dumb/simple router to dispatch application.
+ */
+
+class Router
+{
+    /**
+     * @var array   holds uri divided by /
+     */
+    var $requests = array();
+    // +-------------------------------------------------------------+
+    /**
+     * constructor sets routes from URI if routes not set.
+     * @param array $requests   set routes.
+     */
+    function __construct( $requests=array() ) {
+        if( empty( $requests ) ) {
+            $this->requests = $requests;
+        }
+        else {
+            $this->requests = $this->routeUri();
+        }
+    }
+    // +-------------------------------------------------------------+
+    /**
+     * @return array     returns routes.
+     */
+    function routeUri( $uri=NULL, $script=NULL ) {
+        if( $uri === NULL ) {
+            $uri = explode( '/', $_SERVER[ 'REQUEST_URI' ] );
+        }
+        if( $script === NULL ) {
+            $script = explode( '/', $_SERVER[ 'SCRIPT_NAME' ] );
+        }
+        for( $i= 0; $i < sizeof( $script ); $i++ ) {
+            if( $uri[$i] == $script[$i] ) {
+                array_slice( $uri, 1 );
+            }
+        }
+        return $uri;
+    }
+    // +-------------------------------------------------------------+
+    function dispatch( $dispatcher ) {
+        $dispatcher->dispatch( $this->requests[0], $this->requests );
+    }
+    // +-------------------------------------------------------------+
+}
+
+
+class simpleLoader
+{
+    static $location;
+    static $postfix = '';
+    // +-------------------------------------------------------------+
+    function actionDefault( $ctrl, $requests ) {
+        // loads from existing app file.
+        $action = $requests[0];
+        $extetion = '_' . self::$postfix . '.php';
+        if( file_exists( "{$action}.php" ) ) {
+            include( "{$action}{$extetion}" );
+        }
+        else
+        if( file_exists( "{$action}/app.php" ) ) {
+            include( "{$action}/app{$extetion}" );
+        }
+        $ctrl->nextAct( 'Err404' );
+    }
+    // +-------------------------------------------------------------+
+    function actionErr404( $ctrl, $data ) {
+        // do something about error 404, a file not found.
+    }
+    // +-------------------------------------------------------------+
+}
+
+class simpleView
+{
+    function actionDefault( $ctrl, $data ) {
+        // everything OK.
+    }
+    function actionErr404( $ctrl, $data ) {
+        // show some excuses, or blame user for not finding a page.
+    }
+    function actionException( $ctrl, $data ) {
+        // show some nasty things happened and apologize.
+    }
+}
+
