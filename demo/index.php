fa1e6a033e0ad44f53f72277b762454cfb71e84b
kamei@workspot.jp <asao@asaoMBAir.local>
2012-01-15 22:19:29 +0900

    lots of todo to build better MVC Framework.

--- demo/index.php
+++ demo/index.php
@@ -5,6 +5,27 @@ require_once( '../Viewer.php' );
 require_once( '../Router.php' );
 require_once( '../Dispatch.php' );
 
+/**
+ * TODO: make new Loader
+ *  by inheriting Dispatch.php,
+ *  add routing function to initiate the main loop,
+ *  and add data for controlling a web site, such as
+ *  debug, admin, bread, etc.
+ * TODO: change Loader as Router,
+ *  and merge Router into the above new Loader.
+ * TODO: make Config
+ *  for controlling multiple-lang, admin, debug info,
+ *  dev/staging/real server.
+ * TODO: Loader needs various loading method.
+ *  app.php: load php and prepend model. not sure if
+ *           loader/app.php to prepend...
+ *  action.php: include php and intercept output by ob.
+ *  action.html: read html as a contents.
+ *  action is a file: output as is and set new mime type.
+ * TODO: demo as test site.
+ *  before eating dog food, make test/demo site.
+ * TODO: Debug is... awful. 
+ */
 Debug::w1( 'index.php is here' );
 
 class Loader extends CenaDta\App\Loader {}
